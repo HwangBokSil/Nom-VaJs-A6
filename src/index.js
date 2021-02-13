437743ce7323e9aa348c55ab1291778e96262640
HwangBokSil <66759928+HwangBokSil@users.noreply.github.com>
2021-02-13 22:44:59 +0900

    #1.selfCoding

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+
+const saveName = document.querySelector("select"),
+        value1 = select.value.querySelector("1"),
+        value2 = select.value.querySelector("2"),
+        value3 = select.value.querySelector("3"),
+        value4 = select.value.querySelector("4");
+
+
+function loadName() {
+    const countryName = saveName;
+
+    if ( countryName = value1 ) {
+        console.log("KOR");
+    } else if ( countryName = value2 ) {
+        console.log("GRC");
+    } else if ( countryName = value3 ) {
+        console.log("TUR");
+    } else if ( countryName = value4 ) {
+        console.log("FIN");
+    } else () {
+        console.log("NONE");
+    }
+}
+
+
+function init() {
+    const print
+}
+
+init();
